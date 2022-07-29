d38289ee332b6cd3cc16b7257e04904b5313bf04
Irina <bord.ira@gmail.com>
2022-07-29 23:31:52 +0300

    resolve 8.1 problem skillfactory

---
+++ predict_number.py
@@ -0,0 +1,27 @@
+
+import numpy as np
+
+def random_predict(number:int=np.random.randint(1,101)) -> int:
+    count = 0
+    predict_number_list = list(range(1,101))
+    while True:
+        count+=1
+        predict_number = int(np.mean(predict_number_list))
+        if number != predict_number:
+            if number > np.mean(predict_number):
+                predict_number_list = list(range(predict_number,predict_number_list[-1]+1))
+            else:
+                predict_number_list = list(range(predict_number_list[0],predict_number+1))
+        else:
+            break
+    return count
+
+def score_of_predict():
+    list_results = []
+    for i in range(1000):
+        list_results.append(random_predict())
+    return np.mean(list_results)
+
+print(score_of_predict())
+            
+                  
