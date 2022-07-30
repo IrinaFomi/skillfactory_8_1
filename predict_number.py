f494b0e12cc1e12426152eac80c7e90d35cc1c9c
Irina <bord.ira@gmail.com>
2022-07-30 21:07:07 +0300

    change algorothm to binary search and add readme and requirements

--- predict_number.py
+++ predict_number.py
@@ -2,26 +2,33 @@
 import numpy as np
 
 def random_predict(number:int=np.random.randint(1,101)) -> int:
+    """Guess the number, that passed in argumet.
+
+    Args:
+        number (int, optional): guessed number. Defaults to np.random.randint(1,101).
+
+    Returns:
+        int: the number of attempts when the number was guessed
+    """
     count = 0
-    predict_number_list = list(range(1,101))
+    left_border = 1
+    right_border = 100
+    
+    if number<left_border or number>right_border:
+        raise ValueError(f'Incorrect argument. Must be between [{left_border},{right_border}]')
+    
     while True:
         count+=1
-        predict_number = int(np.mean(predict_number_list))
+        predict_number = int((left_border+right_border)/2)
         if number != predict_number:
             if number > predict_number:
-                predict_number_list = list(range(predict_number+1,predict_number_list[-1]+1))
+                left_border = predict_number + 1
             else:
-                predict_number_list = list(range(predict_number_list[0],predict_number))
+                right_border = predict_number - 1
         else:
             break
+        
     return count
 
-def score_of_predict():
-    list_results = []
-    for i in range(1000):
-        list_results.append(random_predict())
-    return np.mean(list_results)
-
-print(score_of_predict())
             
                   
---
+++ score_of_random_predict.py
@@ -0,0 +1,22 @@
+import numpy as np
+from predict_number import random_predict
+
+
+def score_of_predict():
+    """Score of random predict function :func:`~predict_number.random_predict`
+
+    Returns:
+        float: mean of 1000 calls of random_predict function
+    """
+    list_results = []
+    
+    for i in range(1000):
+        list_results.append(random_predict(np.random.randint(1,101))) 
+    return [min(list_results),np.mean(list_results),max(list_results)]
+
+
+if __name__ == "__main__":
+    # RUN
+    result_score_func = score_of_predict()
+    print(f'Min = {result_score_func[0]}, mean = {result_score_func[1]}, \
+        max = {result_score_func[2]}.')
